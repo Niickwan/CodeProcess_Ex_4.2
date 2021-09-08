ee51de0b4ada1f345339affd6fa042dbf6020076
Niickwan <89967473+Niickwan@users.noreply.github.com>
2021-09-08 11:58:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/*
+*
+* order of code process
+*
+ */
+
+public class Main {
+
+    public static void baffle(String blimp) {               // 8
+        System.out.println(blimp);                          // 9
+        zippo("ping", -5);                       // 10
+    }                                                       // 15
+
+    public static void zippo(String quince, int flag) {     // 3 - 11
+        if (flag < 0) {                                     // 4 - 12
+            System.out.println(quince + " zoop");           // 13
+        } else {                                            // 5
+            System.out.println("ik");                       // 6
+            baffle(quince);                                 // 7
+            System.out.println("boo-wa-ha-ha");             // 16
+        }
+    }                                                       // 14 - 17
+
+    public static void main(String[] args) {                // 1
+        zippo("rattle", 13);                     // 2
+    }                                                       // 18
+}
